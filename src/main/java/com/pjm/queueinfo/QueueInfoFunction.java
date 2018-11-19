e056622af055384abc6df2aa285718bae1722c63
Chaitanya Hazari <chaitanyahazari@Chaitanyas-Air.fios-router.home>
2018-11-18 23:08:06 -0500

    Added SQS (Message Queue)

--- src/main/java/com/pjm/queueinfo/QueueInfoFunction.java
+++ src/main/java/com/pjm/queueinfo/QueueInfoFunction.java
@@ -1,29 +1,48 @@
 package com.pjm.queueinfo;
 
-import com.pjm.queueinfo.model.QueueInfoRequest;
-import com.pjm.queueinfo.model.QueueInfoResponse;
+import com.pjm.queueinfo.request.EmailRequest;
 import com.pjm.queueinfo.request.LexRequest;
 import com.pjm.queueinfo.response.DialogAction;
 import com.pjm.queueinfo.response.LexResponse;
 import com.pjm.queueinfo.response.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
 import java.util.function.Function;
 
-@Component("queueInfoFunction")
+git@Component("queueInfoFunction")
 public class QueueInfoFunction implements Function<LexRequest, LexResponse> {
 
-    @Override
+    @Autowired
+    EmailService emailService;
+
+    @Autowired
+    QueueMessagingTemplate queueMessagingTemplate;
+
     public LexResponse apply(LexRequest lexRequest) {
-        String intentName = lexRequest.getCurrentIntent().getName();
-        lexRequest.getUserId();
+        String email = lexRequest.getInputTranscript();
+
+        EmailRequest emailRequest = new EmailRequest();
+        emailRequest.setTo(email != null ? email : "krishnaha@gmail.com");
+        emailRequest.setBody("Woeful");
+        emailRequest.setSubject("parrot subject");
+
+        queueMessagingTemplate.convertAndSend("https://sqs.us-east-1.amazonaws.com/963929482176/parrot_queue", emailRequest);
+
+        return message("You entered " + email);
+    }
+
+    private LexResponse message(String msg) {
         LexResponse response = new LexResponse();
         DialogAction action = new DialogAction();
-        action.setType("Close");
-        action.setFulfillmentState("Fulfilled");
+        action.setType(DialogAction.CLOSE_TYPE);
+        action.setFulfillmentState(DialogAction.FULFILLMENT_STATE_FULFILLED);
         Message message = new Message();
-        message.setContent(lexRequest.getInputTranscript());
-        message.setContentType("PlainText");
+        message.setContent(msg);
+        message.setContentType(Message.CONTENT_TYPE_PLAIN_TEXT);
         action.setMessage(message);
         response.setDialogAction(action);
         return response;
--- src/main/java/com/pjm/queueinfo/handler/aws/QueueInfoRequestHandler.java
+++ src/main/java/com/pjm/queueinfo/handler/aws/QueueInfoRequestHandler.java
@@ -1,6 +1,5 @@
 package com.pjm.queueinfo.handler.aws;
 
-import com.pjm.queueinfo.model.QueueInfoResponse;
 import com.pjm.queueinfo.request.LexRequest;
 import com.pjm.queueinfo.response.LexResponse;
 import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
